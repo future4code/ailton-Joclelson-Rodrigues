e4564ce049d9f408e8b3126d54fcd2b58e5236b8
Joclelson <jocleelson.159@hotmail.com>
2022-08-11 19:47:22 -0300

    Aula - Aprofundamento em Typescript

---
+++ modulo4/aprofundamento-typescript/src/exercicio4.ts
@@ -0,0 +1,28 @@
+//a)
+type pokemon = {
+	name: string,
+  types: string,
+	healthPoints: number
+}
+
+const pokemon1: pokemon = {
+  name: "Charmander",
+  types: "Fire",
+  healthPoints: 28
+}
+
+const pokemon2: pokemon = {
+  name: "Bulbasaur",
+  types: "Grass/Poison",
+  healthPoints: 31
+}
+
+const pokemon3: pokemon = {
+  name: "Squirtle",
+  types: "Water",
+  healthPoints: 35
+}
+
+//b) tsc && node ./build/exercicio4.js
+//c) mesmo processo
+//d) sim, o comando tsc seguido do nome do arquivo executa a transpilação de todos os arquivos
