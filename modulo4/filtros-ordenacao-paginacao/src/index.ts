21c34d33d89419a6eb1d25ac97e1b729c83dff0b
Joclelson <jocleelson.159@hotmail.com>
2022-08-30 23:57:05 -0300

    Aula - Filtros

--- modulo4/filtros-ordenacao-paginacao/src/index.ts
+++ modulo4/filtros-ordenacao-paginacao/src/index.ts
@@ -1,7 +1,10 @@
 import { app } from "./app";
 import { getAllUsers } from "./endpoints/getAllUsers";
 import { getName } from "./endpoints/getName";
+import { filterUser } from "./endpoints/filterUser";
 
 app.get("/allusers", getName);
 
 app.get("/allusers", getAllUsers);
+
+app.get("/allusers/:type", filterUser);
