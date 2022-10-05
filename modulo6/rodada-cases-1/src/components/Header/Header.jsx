91f50b0823b615ac6c6fdd3d39c085128dd330b0
Joclelson <jocleelson.159@hotmail.com>
2022-10-05 00:16:10 -0300

    :tada: test: Inicio do projeto case da shopper

---
+++ modulo6/rodada-cases-1/src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import { HeaderContainer } from "./styled";
+
+const Header = () => {
+  return (
+    <HeaderContainer>
+      <lu>
+        <li>Pedidos</li>
+        <li></li>
+        <li></li>
+      </lu>
+    </HeaderContainer>
+  );
+};
+export default Header;
---
+++ modulo6/rodada-cases-1/src/components/Header/styled.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const HeaderContainer = styled.header`
+  width: 100%;
+  height: 10vh;
+  border-bottom: 1px solid lightgray;
+  box-shadow: 0px 0px 3px lightgray;
+`;
